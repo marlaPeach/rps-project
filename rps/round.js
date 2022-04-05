6d14680287536fe57489aec2fefe441757325d63
schmidttf14 <schmidttf14@gmail.com>
2022-04-05 00:02:17 -0400

    Initial commit.

--- rps/round.js
+++ rps/round.js
@@ -29,6 +29,20 @@ class Round{
     getComputerSelection() {
         //*********************************ADD CODE HERE *************************************/
         // Use Math.floor and select a random Selections enumerator from above 
+        let random = Math.floor(Math.random() * 3); //Get a random number from 0-2.
+        switch (random)
+        {
+            case 0:
+                return Selections.ROCK;
+                break;
+            case 1:
+                return Selections.PAPER;
+                break;
+            case 2:
+                return Selections.SCISSORS;
+                 break;
+
+        }
     }
 
     /**
@@ -40,6 +54,42 @@ class Round{
     determineWinner() {
         //*********************************ADD CODE HERE *************************************/
         // Use this.playerSelection and this.computerSelection to return the appropriate outcome from the Outcomes enumerator above.
+        if (this.playerSelection === "rock" && this.computerSelection === "paper")
+        {
+            game.incrementComputerWins();
+            return Outcomes.COMPUTER_WINS;
+        } else if(this.playerSelection === "paper" && this.computerSelection === "scissors"){
+            game.incrementComputerWins();
+            return Outcomes.COMPUTER_WINS;
+        }
+        else if(this.playerSelection === "scissors" && this.computerSelection === "rock"){
+            game.incrementComputerWins();
+            return Outcomes.COMPUTER_WINS;
+        }
+        else if(this.playerSelection === "rock" && this.computerSelection === "rock"){
+            game.incrementCountOfTies();
+            return Outcomes.TIE;
+        }
+        else if(this.playerSelection === "scissors" && this.computerSelection === "scissors"){
+            game.incrementCountOfTies();
+            return Outcomes.TIE;
+        }
+        else if(this.playerSelection === "paper" && this.computerSelection === "paper"){
+            game.incrementCountOfTies();
+            return Outcomes.TIE;
+        }
+        else if(this.playerSelection === "paper" && this.computerSelection === "rock"){
+            game.incrementPlayerWins();
+            return Outcomes.PLAYER_WINS;
+        }
+        else if(this.playerSelection === "rock" && this.computerSelection === "scissors"){
+            game.incrementPlayerWins();
+            return Outcomes.PLAYER_WINS;
+        }
+        else if(this.playerSelection === "scissors" && this.computerSelection === "paper"){
+            game.incrementPlayerWins();
+            return Outcomes.PLAYER_WINS;
+        }
     }
 
 
